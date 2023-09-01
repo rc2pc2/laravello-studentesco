221c15ce84781dfdb38345e6daa5dd8778ae5336
rc2pc2 <petriccarcc@gmail.com>
2023-09-01 10:51:53 +0200

    added database seeder for project resource

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,16 +2,23 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i=0; $i < 100 ; $i++) {
+            $project = new Project();
+            $project->name = $faker->unique()->realText($faker->numberBetween(10, 20));
+            $project->description = $faker->unique()->realText($faker->numberBetween(200, 400));
+            $project->save();
+        }
     }
 }
