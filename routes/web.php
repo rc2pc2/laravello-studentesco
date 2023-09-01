90b31b33ee749a1578f57f1b3f7971ba3eb9ee24
rc2pc2 <petriccarcc@gmail.com>
2023-09-01 10:51:26 +0200

    added resource posts routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\ProjectController as AdminProjectController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::prefix('admin')->name('admin.')->middleware('auth')->group( function() {
+    Route::resource('/projects', AdminProjectController::class);
+});
